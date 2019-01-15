3cb4a6014024f2de1add409156c55a2a3113bfb2
github username <github email address>
2019-01-14 19:13:41 -0700

    add seeds

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,18 @@
 class Word < ApplicationRecord
+  validates :letters, presence: true
+
+  @@dictionary = {}
+
+  def add_to_dictionary(sorted, word)
+    if @@dictionary[sorted]
+     @@dictionary[sorted].include?(word.letters) ? @@dictionary[sorted] << word.letters : nil
+    else
+     @@dictionary[sorted] = [word.letters]
+    end
+  end
+
+  def sorted
+    self.letters.chars.sort.join
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+["dare", "read", "dear", "cat", "act", "bat"].each do |word|
+  new_word = Word.create(letters: word)
+  add_to_dictionary(new_word.sorted, new_word)
+end
