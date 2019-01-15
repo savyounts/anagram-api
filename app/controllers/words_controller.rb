285b6581132687352b1d30f685e0849e88735bd3
github username <github email address>
2019-01-15 10:32:45 -0700

    update routes to take params for :letters instead of :id

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -47,7 +47,7 @@ class WordsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_word
-      @word = Word.find_by(params[:letters])
+      @word = Word.find_by(letters: params[:letters])
     end
 
     # Only allow a trusted parameter "white list" through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :words
+  resources :words, param: :letters
 
-  get 'anagrams/:letters', :to => 'words#anagrams'
+  get '/anagrams/:letters', :to => 'words#anagrams'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
